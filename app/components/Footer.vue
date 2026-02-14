df9d9bcbf681a0b9334d7b547ffa423981492a83
bryxxd <brixdesalit7@gmail.com>
2026-02-14 10:08:30 +0800

    About page

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,10 +1,11 @@
 <script setup></script>
 
 <template>
-    
+
   <footer class="px-5 pt-10 pb-5 text-paragraph-1">
     <div class="lg:flex lg:justify-between">
-      <h2 class="text-footer-heading font-tiltWarp font-bold leading-none mix-blend-plus-lighter">Grain<br class="hidden lg:block"> Archive</h2>
+      <h2 class="text-footer-heading font-tiltWarp font-bold leading-none mix-blend-plus-lighter">Grain<br
+          class="hidden lg:block"> Archive</h2>
 
       <div class="w-full text-paragraph-4 mt-20 md:flex md:justify-between lg:flex-col lg:gap-[100px] lg:max-w-[360px]">
         <div class="grid grid-cols-2 md:grid-cols-[140px_140px] gap-10">
@@ -50,7 +51,8 @@
             </ul>
           </div>
         </div>
-        <p class="text-sm md:text-right mt-20 md:mt-0">©2025 Grain Archive<br>Words, images, and signals from the edge</p>
+        <p class="text-sm md:text-right mt-20 md:mt-0">©2025 Grain Archive<br>Words, images, and signals from the edge
+        </p>
       </div>
 
     </div>
