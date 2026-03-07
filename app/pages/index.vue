67beaa0281f651a555a56935f11c4488f24c3117
bryxxd <brixdesalit7@gmail.com>
2026-03-07 12:49:13 +0800

    Added fallback mv height value

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,7 +22,7 @@ const { data: articles } = await useAsyncData('articles-home', () => getArticles
 <template>
   <section class="mv">
     <div class="layout relative">
-      <video autoplay muted loop playsinline class="rounded-lg w-full h-[calc(100vh-60px)] md:h-[calc(100vh-70px)] object-cover">
+      <video autoplay muted loop playsinline class="rounded-lg w-full md:h-[calc(100vh-70px)] object-cover" style="height: calc(100vh - 60px); height: calc(100svh - 60px);">
         <source src="~/assets/video/mv-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
