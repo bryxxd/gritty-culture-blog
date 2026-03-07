6e3daa995c512b000bc8284d5b6bcc7201f87c9d
bryxxd <brixdesalit7@gmail.com>
2026-03-07 12:19:31 +0800

    Fixed layout

--- app/pages/article/[slug].vue
+++ app/pages/article/[slug].vue
@@ -28,7 +28,7 @@ const { data: moreArticles } = await useAsyncData(
       :title="article?.title"
     />
     <p
-      class="text-center font-instrumentSerif italic text-[clamp(40px,8vw,80px)] flex items-start justify-center gap-6"
+      class="text-center font-instrumentSerif italic text-[clamp(25px,8vw,80px)] flex items-start justify-center gap-6"
     >
       <span>{{ article?.terms.nodes[0]?.name }}</span>
       <span>·</span>
